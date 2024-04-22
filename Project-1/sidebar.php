587fac8f6c5c4a5f1af299c67dacb3ec910e3264
muhamadnurraply <146099712+muhamadnurraply@users.noreply.github.com>
2024-04-22 15:56:44 +0700

    Update sidebar.php

--- Project-1/sidebar.php
+++ Project-1/sidebar.php
@@ -1,10 +1,15 @@
+<?php 
+session_start();
+if(!$_SESSION['user']){
+    header("location:login.php");
+}
+?>
 <!-- Main Sidebar Container -->
-
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">Puskesmas Alfatih</span>
     </a>
 
     <!-- Sidebar -->
@@ -15,7 +20,7 @@
                 <img src="dist/img/avatar4.png" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Muhamad Nur Raply</a>
+                <a href="#" class="d-block"><?= $_SESSION['user']['username'];?></a>
             </div>
         </div>
 
