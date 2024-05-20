07075ee8144fb4ca6e2d2445d7c8101121e954ee
muhamadnurraply <146099712+muhamadnurraply@users.noreply.github.com>
2024-05-20 12:54:50 +0700

    update

--- praktikum-09/routes/web.php
+++ praktikum-09/routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace app\Http\Controllers;
-
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin',[Admincontroller::class,'index']);
+Route::get('/kabar', function () {
+    return view('kondisi');
+});
+
+Route::get('/pasien', function () {
+    return view('pasien');
+});
+
+Route::get('/admin', [AdminController::class, 'index']);
